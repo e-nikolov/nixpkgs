8b4e7194871f069dd592e65f4461489e4a2c5efe
Bobby Rong <rjl931189261@126.com>
2022-11-17 09:24:39 +0800

    cinnamon.cinnamon-screensaver: 5.4.4 -> 5.6.0
    This seems to fix "Fractional scaling resets on resume after suspend".
    
    https://github.com/linuxmint/cinnamon-screensaver/compare/5.4.4...5.6.0

--- pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
+++ pkgs/desktops/cinnamon/cinnamon-screensaver/default.nix
@@ -29,13 +29,13 @@
 
 stdenv.mkDerivation rec {
   pname = "cinnamon-screensaver";
-  version = "5.4.4";
+  version = "5.6.0";
 
   src = fetchFromGitHub {
     owner = "linuxmint";
     repo = pname;
     rev = version;
-    hash = "sha256-D+SpAO4i4KGFWJI94LalTMB3j1YPvV63cKb34FDDprk=";
+    hash = "sha256-rbWa+RmaecDiChj2RHBfHbnjJFNFy58NUZ1UxbDsKfk=";
   };
 
   nativeBuildInputs = [
